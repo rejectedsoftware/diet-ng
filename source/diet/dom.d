41a233de3bc39fed72fe2b9264effd1ec14f9c7d
Sönke Ludwig <sludwig@rejectedsoftware.com>
2015-08-02 09:06:17 +0200

    Initial version with basic parser implementation.

---
+++ source/diet/dom.d
@@ -0,0 +1,123 @@
+module diet.dom;
+
+import diet.internal.string;
+
+
+class Node {
+	Location loc;
+	string name;
+	Attribute[] attributes;
+	NodeContent[] contents;
+	NodeAttribs attribs;
+
+	this(Location loc = Location.init, string name = null, Attribute[] attributes = null, NodeContent[] contents = null, NodeAttribs attribs = NodeAttribs.none)
+	{
+		this.loc = loc;
+		this.name = name;
+		this.attributes = attributes;
+		this.contents = contents;
+		this.attribs = attribs;
+	}
+
+	void addText(string text, in ref Location loc)
+	{
+		if (contents.length && contents[$-1].kind == NodeContent.Kind.text && contents[$-1].loc == loc)
+			contents[$-1].contents ~= text;
+		else contents ~= NodeContent.text(text, loc);
+	}
+
+	void stripIfOnlyWhitespace()
+	{
+		if (contents.length == 1 && contents[0].kind == NodeContent.Kind.text && contents[0].contents.ctstrip.length == 0)
+			contents = null;
+	}
+
+	void stripLeadingWhitespace()
+	{
+		while (contents.length >= 1 && contents[0].kind == NodeContent.Kind.text) {
+			contents[0].contents = ctstripLeft(contents[0].contents);
+			if (contents[0].contents.length == 0)
+				contents = contents[1 .. $];
+			else break;
+		}
+	}
+
+	void stripTrailingWhitespace()
+	{
+		while (contents.length >= 1 && contents[$-1].kind == NodeContent.Kind.text) {
+			contents[$-1].contents = ctstripRight(contents[$-1].contents);
+			if (contents[$-1].contents.length == 0)
+				contents = contents[0 .. $-1];
+			else break;
+		}
+	}
+
+	override string toString() const {
+		import std.string : format;
+		return format("Node(%s, %s, %s, %s, %s)", this.tupleof);
+	}
+
+	override bool opEquals(Object other_) {
+		auto other = cast(Node)other_;
+		if (!other) return false;
+		return this.tupleof == other.tupleof;
+	}
+}
+
+enum NodeAttribs {
+	none = 0,
+	translated = 1<<0, /// Translate node contents
+	textNode = 1<<1    /// 
+}
+
+struct Attribute {
+	string name;
+	string expression;
+	AttributeContent[] value;
+}
+
+struct AttributeContent {
+	enum Kind {
+		text,
+		interpolation,
+		rawInterpolation
+	}
+
+	Kind kind;
+	string contents;
+
+	static AttributeContent text(string text) { return AttributeContent(Kind.text, text); }
+	static AttributeContent interpolation(string expression) { return AttributeContent(Kind.interpolation, expression); }
+	static AttributeContent rawInterpolation(string expression) { return AttributeContent(Kind.rawInterpolation, expression); }
+}
+
+struct NodeContent {
+	enum Kind {
+		node,
+		text,
+		expression,
+		interpolation,
+		rawInterpolation
+	}
+
+	Kind kind;
+	Location loc;
+	Node node;
+	string contents;
+
+	static NodeContent tag(Node node) { return NodeContent(Kind.node, node.loc, node); }
+	static NodeContent text(string text, Location loc) { return NodeContent(Kind.text, loc, Node.init, text); }
+	static NodeContent expression(string text, Location loc) { return NodeContent(Kind.expression, loc, Node.init, text); }
+	static NodeContent interpolation(string text, Location loc) { return NodeContent(Kind.interpolation, loc, Node.init, text); }
+	static NodeContent rawInterpolation(string text, Location loc) { return NodeContent(Kind.rawInterpolation, loc, Node.init, text); }
+
+	bool opEquals(in ref NodeContent other)
+	{
+		return this.kind == other.kind && this.loc == other.loc && this.node == other.node && this.contents == other.contents;
+	}
+}
+
+struct Location {
+	string file;
+	int line;
+}
